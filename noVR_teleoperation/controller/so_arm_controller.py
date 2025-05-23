9e5dccf03eeaa03191379988d2f4affe125b08f7
drenayaz <coraliedeplanne@live.fr>
2025-05-23 15:46:33 +0200

    remove pypot and use rustypot instead

--- noVR_teleoperation/controller/so_arm_controller.py
+++ noVR_teleoperation/controller/so_arm_controller.py
@@ -2,7 +2,7 @@ import time
 
 import numpy as np
 from controller.controller import Controller  # type: ignore
-from controller.feetech_rustypot import FeetechSOArm  # type: ignore
+from controller.feetech_rustypot import Feetech  # type: ignore
 from scipy.spatial.transform import Rotation as R  # type: ignore
 from trackers.tracker import TrackerType  # type: ignore
 from utils import make_homogenous_matrix_from_rotation_matrix  # type: ignore
@@ -12,7 +12,7 @@ class SoArmController(Controller):
     def __init__(self, port):
         super().__init__()
         self.tracker_type = TrackerType.SO_ARM
-        self.so_arm = FeetechSOArm(port, [1, 2, 3, 4, 5, 6])
+        self.so_arm = Feetech(port, [1, 2, 3, 4, 5, 6])
 
         self.so_init_joints = {
             "r_arm": np.deg2rad([-58.33, -7.16, 27.74, 72.48, -61.67, -1.8]),
